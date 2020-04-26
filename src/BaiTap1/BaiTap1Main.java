120ebc6f30730d14d956e6b04737a1f74c182ae9
tranthithachthao <46111107+tranthithachthao@users.noreply.github.com>
2020-04-26 12:32:33 +0700

    v1

---
+++ src/BaiTap1/BaiTap1Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap1;
+
+/**
+ *
+ * @author Dell
+ */
+public class BaiTap1Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
